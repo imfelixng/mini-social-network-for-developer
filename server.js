8830d5265cddd3acd520a08564d56c576e95521b
Nguyen Quang An <ngquangan@gmail.com>
2018-12-14 12:56:57 +0700

    add model for user

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = process.eventNames.port | 5000;
+const port = process.env.port | 5000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
