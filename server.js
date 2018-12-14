bbf4be27a16559048277c380f1b86cbb312a9387
Nguyen Quang An <ngquangan@gmail.com>
2018-12-14 22:00:41 +0700

    add Procfile and define PORT default

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = process.env.port | 5000;
+const port = process.env.PORT | 5000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
